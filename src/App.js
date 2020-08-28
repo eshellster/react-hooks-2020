333bf76f176ae7613ce1b1680a0fdcef702b2002
eshell <eshellster@gmail.com>
2020-08-28 16:14:59 +0900

    App에서 메세지 부분만 분리

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 import { useForm } from "./useForm";
-import { useFetch } from "./useFetch";
+// import { useFetch } from "./useFetch";
 import { Hello } from "./Hello";
 
 const App = () => {
   const [values, handleChange] = useForm({ name: "", email: "", password: "" });
-  const [count, setCount] = useState(JSON.parse(localStorage.getItem("count")));
-  const { data } = useFetch(`http://numbersapi.com/${count}/trivia`);
+
   const inputRef = useRef();
 
   const [showHello, setHello] = useState(true);
 
-  useEffect(() => {
-    localStorage.setItem("count", JSON.stringify(count));
-  }, [count]);
-
   return (
     <div className="parent">
       <div className="containerWrap">
-        <div>{data ? data : `loading...`}</div>
-        <button
-          onClick={() => {
-            setCount((c) => c + 1);
-          }}
-        >
-          increment
-        </button>
         <div>
-          <button onClick={() => setHello(!showHello)}>toggle</button>
           {showHello && <Hello />}
+          <button onClick={() => setHello(!showHello)}>toggle</button>
+        </div>
+        <div>
           <input
             ref={inputRef}
             type="email"
--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,33 @@
-import React, { useRef } from "react";
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
 
 export const Hello = () => {
-  const renders = useRef(0);
-  console.log("hello render:", renders.current++);
-  return <div>hello</div>;
+  //   const renders = useRef(0);
+  const [count, setCount] = useState(JSON.parse(localStorage.getItem("count")));
+  const { data } = useFetch(`http://numbersapi.com/${count}/trivia`);
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count));
+  }, [count]);
+  //   console.log("hello render:", renders.current++);
+  return (
+    <div>
+      <div>{data ? data : `loading...`}</div>
+      <div>count: {count}</div>
+      <button
+        onClick={() => {
+          setCount((c) => c + 1);
+        }}
+      >
+        increment
+      </button>
+      <button
+        onClick={() => {
+          setCount(1);
+        }}
+      >
+        reset
+      </button>
+      <hr />
+    </div>
+  );
 };
