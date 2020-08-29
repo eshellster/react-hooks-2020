e6fce627acd611a7f9d6c37d8cbeba9ba73657f4
eshell <eshellster@gmail.com>
2020-08-29 17:48:34 +0900

    실습준비

--- src/App.js
+++ src/App.js
@@ -1,66 +1,16 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useCallback } from "react";
 import "./App.css";
-import { useForm } from "./useForm";
-// import { useFetch } from "./useFetch";
 import { Hello } from "./Hello";
-import { useMeasure } from "./useMeasure";
 
 const App = () => {
-  const [values, handleChange] = useForm({ name: "", email: "", password: "" });
+  const [count, setCount] = useState(0);
 
-  const inputRef = useRef();
-  // const hello = useRef(() => console.log("hello"));
-
-  const [showHello, setHello] = useState(true);
-
-  // useLayoutEffect(() => {
-  //   console.log(inputRef.current.getBoundingClientRect());
-  // }, []);
-
-  const [rect, inputRef2] = useMeasure([]);
+  const increment = useCallback(() => setCount(count + 1), [count, setCount]);
 
   return (
-    <div className="parent">
-      <div className="containerWrap">
-        <div>
-          {showHello && <Hello />}
-          <button onClick={() => setHello(!showHello)}>toggle</button>
-        </div>
-        <div>
-          <input
-            ref={inputRef}
-            type="email"
-            name="email"
-            placeholder="email"
-            value={values.email}
-            onChange={handleChange}
-          />
-          <input
-            ref={inputRef2}
-            type="name"
-            name="name"
-            placeholder="name"
-            value={values.name}
-            onChange={handleChange}
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="password"
-            value={values.password}
-            onChange={handleChange}
-          />
-          <button
-            onClick={() => {
-              inputRef.current.focus();
-              // hello.current();
-            }}
-          >
-            focus
-          </button>
-        </div>
-        <pre>{JSON.stringify(rect, null, 2)}</pre>
-      </div>
+    <div>
+      <Hello increment={increment} />
+      <div>count:{count}</div>
     </div>
   );
 };
--- src/Hello.js
+++ src/Hello.js
@@ -1,41 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { useFetch } from "./useFetch";
-import { useMeasure } from "./useMeasure";
+import React from "react";
+import { useCountRenders } from "./useCountRenders";
 
-export const Hello = () => {
-  //   const renders = useRef(0);
-  const [count, setCount] = useState(JSON.parse(localStorage.getItem("count")));
-  const { data } = useFetch(`http://numbersapi.com/${count}/trivia`);
-  useEffect(() => {
-    localStorage.setItem("count", JSON.stringify(count));
-  }, [count]);
-  //   console.log("hello render:", renders.current++);
-
-  const [rect, divRef] = useMeasure([data]);
-
-  return (
-    <div>
-      <div style={{ display: "flex" }}>
-        <div ref={divRef}>{data ? data : `loading...`}</div>
-      </div>
-
-      <pre>{JSON.stringify(rect, null, 2)}</pre>
-      <div>count: {count}</div>
-      <button
-        onClick={() => {
-          setCount((c) => c + 1);
-        }}
-      >
-        increment
-      </button>
-      <button
-        onClick={() => {
-          setCount(1);
-        }}
-      >
-        reset
-      </button>
-      <hr />
-    </div>
-  );
-};
+export const Hello = React.memo(({ increment }) => {
+  useCountRenders();
+  return <button onClick={increment}>hello</button>;
+});
