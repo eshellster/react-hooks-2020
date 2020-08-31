4f7da78612566d2388cc4842bbe3b9c9f3b4d18a
eshell <eshellster@gmail.com>
2020-08-31 12:36:01 +0900

    함수 내부에 있어도 문제 없음...

--- src/App.js
+++ src/App.js
@@ -2,31 +2,31 @@ import React, { useState, useMemo } from "react";
 import "./App.css";
 import { useFetch } from "./useFetch";
 
-function computeLongeWord(arr) {
-  if (!arr) {
-    return [];
-  }
-
-  console.log("computing longest word");
-
-  let longestWord = "";
-
-  JSON.parse(arr).forEach((sentence) =>
-    sentence.split(" ").forEach((word) => {
-      if (word.length > longestWord.length) {
-        longestWord = word;
-      }
-    })
-  );
-
-  return longestWord;
-}
-
 const App = () => {
   const [count, setCount] = useState(0);
   const { data } = useFetch(
     "https://raw.githubusercontent.com/ajzbc/kanye.rest/quotes/quotes.json"
   );
+
+  function computeLongeWord(arr) {
+    if (!arr) {
+      return [];
+    }
+
+    console.log("computing longest word");
+
+    let longestWord = "";
+
+    JSON.parse(arr).forEach((sentence) =>
+      sentence.split(" ").forEach((word) => {
+        if (word.length > longestWord.length) {
+          longestWord = word;
+        }
+      })
+    );
+
+    return longestWord;
+  }
 
   const longestWord = useMemo(() => computeLongeWord(data), [data]);
 
